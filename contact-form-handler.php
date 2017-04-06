51b91b62b02be154446dd410479bb668724c150f
NasriGhassen <ghassen.nasri@esprit.tn>
2017-04-06 01:41:26 +0100

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -7,12 +7,7 @@ $Name = Trim(stripslashes($_POST['name']));
 $Email = Trim(stripslashes($_POST['email'])); 
 $Message = Trim(stripslashes($_POST['message'])); 
 
-// validation
-$validationOK=true;
-if (!$validationOK) {
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
-  exit;
-}
+
 
 // prepare email body text
 $Body = "";
